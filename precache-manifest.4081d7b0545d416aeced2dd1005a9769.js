6fe3fed1d7758e13f899651b1390fd490d37937d
makkiboukhris <makki01boukhris@gmail.com>
2020-11-24 09:46:05 +0100

    Updates

---
+++ precache-manifest.4081d7b0545d416aeced2dd1005a9769.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6949ef13e59b29858f1a50c926ffd587",
+    "url": "/myFrontEnd/index.html"
+  },
+  {
+    "revision": "c5e116f1d0aa9211ba88",
+    "url": "/myFrontEnd/static/css/main.00a89b91.chunk.css"
+  },
+  {
+    "revision": "70d84f07f4dfbb96e8c2",
+    "url": "/myFrontEnd/static/js/2.7e1ad141.chunk.js"
+  },
+  {
+    "revision": "17e60a53a9c1f975602446e917b6f0c1",
+    "url": "/myFrontEnd/static/js/2.7e1ad141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5e116f1d0aa9211ba88",
+    "url": "/myFrontEnd/static/js/main.715cf088.chunk.js"
+  },
+  {
+    "revision": "b491efeb2a8d8f344f7f",
+    "url": "/myFrontEnd/static/js/runtime-main.64eb3e67.js"
+  },
+  {
+    "revision": "7f8369c2922ff4998465b53c74df3b2a",
+    "url": "/myFrontEnd/static/media/BackgroundProfile1.7f8369c2.svg"
+  },
+  {
+    "revision": "655600a5edd9d7c8900bf52744525e8c",
+    "url": "/myFrontEnd/static/media/Card1.655600a5.svg"
+  },
+  {
+    "revision": "9269e061a2d09d849b7d6b178969dddc",
+    "url": "/myFrontEnd/static/media/Card2.9269e061.svg"
+  },
+  {
+    "revision": "a89c64ec23d18fdc3942fd86c3ffe7ad",
+    "url": "/myFrontEnd/static/media/Card3.a89c64ec.svg"
+  },
+  {
+    "revision": "dd883e771912f54c85caf3834448fa9a",
+    "url": "/myFrontEnd/static/media/Card4.dd883e77.svg"
+  },
+  {
+    "revision": "35b19678dfb301c6c0b467ebb05e0b03",
+    "url": "/myFrontEnd/static/media/background1.35b19678.svg"
+  },
+  {
+    "revision": "bb06eb37e41e73edb44be63cd973874e",
+    "url": "/myFrontEnd/static/media/backgroundLOGIN.bb06eb37.svg"
+  },
+  {
+    "revision": "cd87ad53887a8fa028b7319f24d3e593",
+    "url": "/myFrontEnd/static/media/logo.cd87ad53.svg"
+  }
+]);
